ace76f1b7801db08a721fa7445165735448fb561
Tóth Tamás <t.tomi1209@gmail.com>
2026-04-22 19:52:52 +0200

    init.py finished

--- berauto/berautoApp/app/blueprints/user/__init__.py
+++ berauto/berautoApp/app/blueprints/user/__init__.py
@@ -1,5 +1,15 @@
 from apiflask import APIBlueprint
 
-bp = APIBlueprint('user', __name__, tag="user")
+bp = APIBlueprint('main', __name__, tag="main")
 
-from app.blueprints.user import routes
+from app.blueprints.user import bp as user_bp
+bp.register_blueprint(user_bp, url_prefix='/user')
+
+from app.blueprints.car import bp as car_bp
+bp.register_blueprint(car_bp, url_prefix='/car')
+
+from app.blueprints.rental import bp as rental_bp
+bp.register_blueprint(rental_bp, url_prefix='/rental')
+
+from app.blueprints.invoice import bp as invoice_bp
+bp.register_blueprint(invoice_bp, url_prefix='/invoice')
