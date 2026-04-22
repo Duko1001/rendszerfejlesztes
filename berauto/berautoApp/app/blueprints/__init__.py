13e13ff6d00ac875248e043c5e7cbdeff5706e98
dudasp04-hub <dudas.p04@gmail.com>
2026-04-22 20:06:33 +0200

    init.py rossz helyen van

--- berauto/berautoApp/app/blueprints/__init__.py
+++ berauto/berautoApp/app/blueprints/__init__.py
@@ -2,12 +2,14 @@ from apiflask import APIBlueprint
 
 bp = APIBlueprint('main', __name__, tag="main")
 
-@bp.route('/')
-def index():
-    return {"message": "API működik"}
-
 from app.blueprints.user import bp as user_bp
-bp.register_blueprint(user_bp, url_prefix="/user")
+bp.register_blueprint(user_bp, url_prefix='/user')
+
+from app.blueprints.car import bp as car_bp
+bp.register_blueprint(car_bp, url_prefix='/car')
 
 from app.blueprints.rental import bp as rental_bp
-bp.register_blueprint(rental_bp, url_prefix="/rental")
+bp.register_blueprint(rental_bp, url_prefix='/rental')
+
+from app.blueprints.invoice import bp as invoice_bp
+bp.register_blueprint(invoice_bp, url_prefix='/invoice')
